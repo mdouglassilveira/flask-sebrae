5e657395935298f6b40853a5d9795332ff27f19a
mdouglassilveira <maicondsilveira15@gmail.com>
2025-03-12 19:47:33 -0300

    Corrigindo quebras de linhas em startup.sh

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Instalando Google Chrome..."
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-chrome-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+echo "Instalação concluída!"
